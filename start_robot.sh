8e6f8a0675e5726834267eda9a82deaf8a52645c
Yusuf Can Uyar <s_uyar24@stud.hwr-berlin.de>
2026-01-16 12:16:05 +0100

    Create start_robot.sh for ROS Noetic setup

---
+++ start_robot.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# --- LOGGING START ---
+echo "[Autostart] Skript gestartet am $(date)"
+
+# 1. ROS Noetic laden (Ubuntu 20.04)
+source /opt/ros/noetic/setup.bash
+
+# 2. Workspace laden
+# Da dein User 'ubuntu' ist, liegt der Workspace hier:
+source /home/ubuntu/catkin_ws/devel/setup.bash
+
+# 3. Umgebungsvariablen setzen (Wichtig für Display/Remote)
+export ROS_HOME=/home/ubuntu/.ros
+export TERM=xterm
+
+# 4. Starten
+# Paket: signal_project | Datei: state_machine.launch
+echo "[Autostart] Starte roslaunch..."
+roslaunch signal_project state_machine.launch
